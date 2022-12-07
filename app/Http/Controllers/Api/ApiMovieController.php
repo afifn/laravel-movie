24303acba5d25565e1fc349179d4e7beffffe8be
afifn <a@kuycoding.com>
2022-12-07 08:50:45 +0700

    adding API Restful movie

--- app/Http/Controllers/Api/ApiMovieController.php
+++ app/Http/Controllers/Api/ApiMovieController.php
@@ -3,9 +3,39 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class ApiMovieController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $search = $request->query('search');
+        $movies = Movie::where('title', 'like', '%' . $search . '%')
+            ->orderBy('created_at', 'ASC')
+            ->paginate(10);
+        return response()->json([
+            'error' => false,
+            'message' => 'movie fetched success',
+            'data' => $movies
+        ], 200);
+    }
+
+    public function show(Request $request, $id)
+    {
+        $movie = Movie::find($id);
+        if (!$movie) {
+            return response()->json([
+                'error' => true,
+                'message' => 'Movie not found',
+            ], 400);
+        } else {
+
+            return response()->json([
+                'error' => false,
+                'message' => 'Movie fetced success',
+                'data' => $movie,
+            ], 200);
+        }
+    }
 }
